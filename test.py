bd6b0caf7c23c928d5b26e2fb1d37366f014839f
Chris Dobson <chris@penguinleaf.com>
2016-05-20 22:21:35 -0400

    random lightshow!!!

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
-import pyhue
+import pyhue, random, time
 
 bridge = pyhue.Bridge('192.168.1.82', 'JaiTE2tcfSTd2l7A3FKvn1O8sskGYlbxf9fr5dRi')
 
-for light in bridge.lights:
-    print(light)
-    light.on = True
-    light.hue = 0
+while True:
+    try:
+        for light in bridge.lights:
+            i = random.randint(0, 65535)
+            print(i)
+            light.on = True
+            light.hue = i
+    except Exception as e:
+        print e
+        time.sleep(1)
+    else:
+        time.sleep(0.01)
