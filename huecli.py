d125edcbc7ae5c82b96c4e0eaec7886cc4fe8e0a
Kyle Rubenok <kyle@rubenok.ca>
2016-06-03 20:04:58 -0400

    Added a BPM function and some documentation

--- huecli.py
+++ huecli.py
@@ -1,4 +1,6 @@
-#A Command Line Interface (CLI) for Philips Hue using Pyhue and Python.
+#A Command Line Interface (CLI) for Philips Hue using Pyhue and Python. Totally useless but an excuse to get some experiance with Pyhue and such...
+#TODO: make a directory of colours that it can understand, create a clean means of connecting the bridge
+
 import pyhue, sys, random, time
 
 bridge = pyhue.Bridge('192.168.1.32', 'J4wVejuDoOvrW4TlOdGgv-2mrNVokM-0KwGtyaRm')
@@ -19,12 +21,13 @@ while True:
           light.on = True
 
     elif command == "party":
+        bpm = int(raw_input("Enter the BPM of your party: "))
         while True:
             for light in bridge.lights :
-                colour = random.randint(0, 65535)
-                setLightHue(light,colour)
-                print("Set " +str(colour))
-                time.sleep(1)
+               colour = random.randint(0, 65535)
+               setLightHue(light,colour)
+               print("Set "+str(colour))
+               time.sleep(2/bpm)
 
     elif command == "exit":
         sys.exit()
